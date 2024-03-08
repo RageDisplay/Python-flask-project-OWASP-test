f860de50e23e92fa620d4960570ebef09bd4222f
Dmiriy <dimshvetsov@gmail.com>
2024-03-08 18:11:05 +0300

    vis

--- TESTcreateusers.py
+++ TESTcreateusers.py
@@ -25,6 +25,14 @@ hash_pass1 = sha256(pass1.encode()).hexdigest()
 hash_pass2 = sha256(pass2.encode()).hexdigest()
 hash_pass3 = sha256(pass3.encode()).hexdigest()
 
-add_user(name1, hash_name1 + hash_pass1)
-add_user(name2, hash_name2 + hash_pass2)
-add_user(name3, hash_name3 + hash_pass3)
+hash_namesum1 = hash_name1 + hash_pass1
+hash_namesum2 = hash_name2 + hash_pass2
+hash_namesum3 = hash_name3 + hash_pass3
+
+extrahash1 = sha256(hash_namesum1.encode()).hexdigest()
+extrahash2 = sha256(hash_namesum2.encode()).hexdigest()
+extrahash3 = sha256(hash_namesum3.encode()).hexdigest()
+
+add_user(name1, extrahash1)
+add_user(name2, extrahash2)
+add_user(name3, extrahash3)
--- XSSTokenTest.py
+++
@@ -1,5 +0,0 @@
-import html
-
-user_input = "<script>alert('XSS');</script>"
-escaped_input = html.escape(user_input)
-print(escaped_input)
